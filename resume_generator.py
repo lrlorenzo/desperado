6259e9d7021434a7c7abac82d033c4476330101c
Loyce <lrlorenzo@gmail.com>
2026-04-01 10:06:38 +0800

    initial load

--- resume_generator.py
+++ resume_generator.py
@@ -41,7 +41,7 @@ class ResumeGenerator:
                         _logger.debug(f"DataFrame shape: {df.shape} Columns: {df.columns.tolist()}")
                         for row in df.itertuples(index=True, name="Row"):                          
                             _logger.debug(f"Processing row: {row.Index} {row.First_Name} {row.Last_Name}")
-                            skills_list = [s.strip() for s in row.Technical_Skills.split(",")]
+                            skills_list = [s.strip() for s in row.Technical_Skills.splitlines() if s.strip()]
                             tech_skills_chunk = self.chunk_list(skills_list, 3)
 
                             certifications_list = [c.strip() for c in row.Certifications.splitlines() if c.strip()]
@@ -54,10 +54,25 @@ class ResumeGenerator:
                                 "Email_Address": row.Email_Address,
                                 "Address": row.Address,
                                 "Summary": row.Summary,
+                                "Job_Title1": row.Job_Title1,
+                                "Job_Company1": row.Job_Company1,
+                                "Job_Employement_Date1": row.Job_Employement_Date1,
                                 "Job_Summary1": row.Job_Summary1,
+                                "Job_Title2": row.Job_Title2,
+                                "Job_Company2": row.Job_Company2,
+                                "Job_Employement_Date2": row.Job_Employement_Date2,
                                 "Job_Summary2": row.Job_Summary2,
+                                "Job_Title3": row.Job_Title3,
+                                "Job_Company3": row.Job_Company3,
+                                "Job_Employement_Date3": row.Job_Employement_Date3,
                                 "Job_Summary3": row.Job_Summary3,
+                                "Job_Title4": row.Job_Title4,
+                                "Job_Company4": row.Job_Company4,
+                                "Job_Employement_Date4": row.Job_Employement_Date4,
                                 "Job_Summary4": row.Job_Summary4,
+                                "Job_Title5": row.Job_Title5,
+                                "Job_Company5": row.Job_Company5,
+                                "Job_Employement_Date5": row.Job_Employement_Date5,                                
                                 "Job_Summary5": row.Job_Summary5,
                                 "Education": row.Education,
                                 "Certifications": certifications_list,
